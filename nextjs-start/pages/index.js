809cf078f29da3d4fff6c57ddeccc9a2686d4350
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-07-12 23:08:15 +0900

    Routing
    Routing
    페이지 간 클라이언트 측 경로 전환을 활성화하고 single-page app 경험을 제공하려면 Link컴포넌트가 필요하다.
    
    // 변경 전
    < a href="/about">About Us< /a>
    
    // 변경 후
    import Link from 'next/link'
    
    < Link href="/about">
    < a>About Us< /a>
    < /Link>
    ```
    https://nextjs.org/docs/messages/no-html-link-for-pages
    
    useRouter()
    앱의 함수 컴포넌트에서 router객체 내부에 접근하려면 userRouter()훅을 사용할 수 있다.
    useRouter는 React Hook이다. 즉, 클래스와 함께 사용할 수 없다. withRouter를 사용하거나 클래스를 함수 컴포넌트로 래핑할 수 있다.
    ```
    const router = useRouter()
    ```
    https://nextjs.org/docs/api-reference/next/router#userouter

--- nextjs-start/pages/index.js
+++ nextjs-start/pages/index.js
@@ -1,3 +1,22 @@
+// import { useState } from "react";
+
+import NavBar from "../components/NavBar";
+
+// export default function Home() {
+//   const [counter, setCounter] = useState(0);
+//   return (
+//     <div>
+//       <h1>Hello {counter}</h1>
+//       <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+//     </div>
+//   );
+// }
+
 export default function Home() {
-  return "hi";
+  return (
+    <div>
+      <NavBar />
+      <h1>Hello</h1>
+    </div>
+  );
 }
