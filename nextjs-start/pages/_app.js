248ef89fd19d5168b0e9833345b4f919c4808528
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-07-16 17:55:22 +0900

    Nextjs 간단 동작 구조
    Nextjs 간단 동작 구조
    - Nextjs가 페이지를 사전 생성한다, 그건 HTML페이지 이고
    유저는 HTML페이지를 바로 보기 때문에 백지화면이나 로딩구간이 없다.
    하지만 유저가 모든 자바스크립트를 다운로드 한 후에는
    ReactJS가 프론트엔드에 나타나 다시 주도권을 가져와서 모든게 일반적인 ReactJS처럼 동작한다.
    그래서 useState같은 ReactJS의 모든 걸 사용할 수 있다.

--- nextjs-start/pages/_app.js
+++ nextjs-start/pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
       <style jsx global>
         {`
           a {
-            color: white;
+            color: green;
           }
         `}
       </style>
