46b60a46ee40f8e252ef20565241b99a436fb59f
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-07-27 21:24:07 +0900

    Server Side Rendering(SSR)
    getServerSideProps 활용
    - page에서 서버 측 랜더링 함수인 getServerSideProps함수를 export하는 경우 Next.js는 getServerSideProps에서 반환된 데이터를 사용하여 각 request에서 이 페이지를 pre-render합니다. getServerSideProps는 서버 측에서만 실행되며 브라우저에서는 실행되지 않습니다.

--- nextjs-start/pages/index.js
+++ nextjs-start/pages/index.js
@@ -12,23 +12,24 @@ import Seo from "../components/Seo";
 //   );
 // }
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })(); // (async)() - IIFE (즉시 실행 함수 표현, Immediately Invoked Function Expression) 이라고 찾았습니다.
-    // 첫번째 괄호는 익명함수를 감싸 실행 될 함수가 전역 스코프에 불필요한 변수를 추가하거나,
-    // IIFE 내부안으로 다른 변수들이 접근하는 것을 막을 수 있는 방법이라고 설명되어 있습니다.
-    // 두번째 괄호는 즉시 실행 함수를 생상하는 괄호이고,
-    // 이를 통해 자바스크립트 엔진은 함수를 즉시 해석해서 실행한다고 설명되어 있습니다.;
-  }, []);
+export default function Home({ results }) {
+  // const [movies, setMovies] = useState();
+  // useEffect(() => {
+  //   (async () => {
+  //     const { results } = await (await fetch(`/api/movies`)).json();
+  //     setMovies(results);
+  //   })(); // (async)() - IIFE (즉시 실행 함수 표현, Immediately Invoked Function Expression) 이라고 찾았습니다.
+  // 첫번째 괄호는 익명함수를 감싸 실행 될 함수가 전역 스코프에 불필요한 변수를 추가하거나,
+  // IIFE 내부안으로 다른 변수들이 접근하는 것을 막을 수 있는 방법이라고 설명되어 있습니다.
+  // 두번째 괄호는 즉시 실행 함수를 생상하는 괄호이고,
+  // 이를 통해 자바스크립트 엔진은 함수를 즉시 해석해서 실행한다고 설명되어 있습니다.;
+  // }, []);
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {/* {!movies && <h4>Loading...</h4>} */}
+      {/* {movies?.map((movie) => ( */}
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -58,4 +59,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
